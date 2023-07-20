c4228c3cd46b813fe07776e02ed97c56b44a1b85
Antcating <evgenij.fedorchenko@gmail.com>
2023-07-20 22:21:45 +0300

    refactor: :hammer: Added API descriptions

--- src/TelegramCrawlerAPI/crud.py
+++ src/TelegramCrawlerAPI/crud.py
@@ -3,15 +3,6 @@ from sqlalchemy.orm import Session
 from . import models
 
 def get_channel_by_id(db: Session, channel_id: int):
-    """Returns exact channel with given id
-
-    Args:
-        db (Session): SQLAlchemy Session
-        channel_title (int): Channel id
-
-    Returns:
-        models.TelegramChannel: Returns single query channel result
-    """
     return (
         db.query(models.TelegramChannel)
         .filter(models.TelegramChannel.id == channel_id)
@@ -19,15 +10,6 @@ def get_channel_by_id(db: Session, channel_id: int):
     )
 
 def get_channel_by_username(db: Session, channel_username: str):
-    """Returns channel with given username (using ilike query)
-
-    Args:
-        db (Session): SQLAlchemy Session
-        channel_username (str): Channel username
-
-    Returns:
-        models.TelegramChannel: Returns single query channel result
-    """
     return (
         db.query(models.TelegramChannel)
         .filter(models.TelegramChannel.username.ilike(f'%{channel_username}%'))
@@ -35,16 +17,6 @@ def get_channel_by_username(db: Session, channel_username: str):
     )
 
 def get_channels_by_username(db: Session, channel_username: str, limit: int = 3):
-    """Returns list of channels with similar to given username (using ilike query)
-
-    Args:
-        db (Session): SQLAlchemy Session
-        channel_username (str): Channel username
-        limis: (int): Limits number of channels in output
-
-    Returns:
-        list(models.TelegramChannel): Returns list of channels from query result
-    """
     return (
         db.query(models.TelegramChannel)
         .filter(models.TelegramChannel.username.ilike(f'%{channel_username}%'))
@@ -54,15 +26,6 @@ def get_channels_by_username(db: Session, channel_username: str, limit: int = 3)
 
 
 def get_channel_by_title(db: Session, channel_title: str):
-    """Returns channel with given title (using ilike query)
-
-    Args:
-        db (Session): SQLAlchemy Session
-        channel_title (str): Channel title
-
-    Returns:
-        models.TelegramChannel: Returns single query channel result
-    """
     return (
         db.query(models.TelegramChannel)
         .filter(models.TelegramChannel.title.ilike(f'%{channel_title}%'))
@@ -71,16 +34,6 @@ def get_channel_by_title(db: Session, channel_title: str):
 
 
 def get_channels_by_title(db: Session, channel_title: str, limit: int = 3):
-    """Returns list of channels with similar to given title (using ilike query)
-
-    Args:
-        db (Session): SQLAlchemy Session
-        channel_username (str): Channel username
-        limis: (int): Limits number of channels in output
-
-    Returns:
-        list(models.TelegramChannel): Returns list of channels from query result
-    """
     return (
         db.query(models.TelegramChannel)
         .filter(models.TelegramChannel.title.ilike(f'%{channel_title}%'))
--- src/TelegramCrawlerAPI/main.py
+++ src/TelegramCrawlerAPI/main.py
@@ -19,6 +19,15 @@ def get_db():
 
 @app.get("/channel_by_id/{channel_id}/", response_model=schemas.Channel)
 def get_channel_by_id(channel_id: int, db: Session = Depends(get_db)):
+    """Returns exact channel with given id
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_title (int): Channel id
+
+    Returns:
+        models.TelegramChannel: Returns single query channel result
+    """
     db_channel = crud.get_channel_by_id(db, channel_id)
     if db_channel is None:
         raise HTTPException(status_code=404, detail="No channel was found")
@@ -26,6 +35,15 @@ def get_channel_by_id(channel_id: int, db: Session = Depends(get_db)):
 
 @app.get("/channel_by_username/{channel_username}/", response_model=schemas.Channel)
 def get_channel_by_username(channel_username: str, db: Session = Depends(get_db)):
+    """Returns channel with given username (using ilike query)
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_username (str): Channel username
+
+    Returns:
+        models.TelegramChannel: Returns single query channel result
+    """
     db_channel = crud.get_channel_by_username(db, channel_username)
     if db_channel is None:
         raise HTTPException(status_code=404, detail="No channel was found")
@@ -33,6 +51,16 @@ def get_channel_by_username(channel_username: str, db: Session = Depends(get_db)
 
 @app.get("/channels_by_username/{channel_username}/", response_model=list[schemas.Channel])
 def get_channels_by_username(channel_username: str, db: Session = Depends(get_db), limit: int = 3):
+    """Returns list of channels with similar to given username (using ilike query)
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_username (str): Channel username
+        limis: (int): Limits number of channels in output
+
+    Returns:
+        list(models.TelegramChannel): Returns list of channels from query result
+    """
     db_channels = crud.get_channels_by_username(db, channel_username, limit)
     if db_channels is None:
         raise HTTPException(status_code=404, detail="No channel was found")
@@ -40,6 +68,15 @@ def get_channels_by_username(channel_username: str, db: Session = Depends(get_db
 
 @app.get("/channel_by_title/{channel_title}/", response_model=schemas.Channel)
 def get_channel_by_title(channel_title: str, db: Session = Depends(get_db)):
+    """Returns channel with given title (using ilike query)
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_title (str): Channel title
+
+    Returns:
+        models.TelegramChannel: Returns single query channel result
+    """
     db_channel = crud.get_channel_by_title(db, channel_title)
     if db_channel is None:
         raise HTTPException(status_code=404, detail="No channel was found")
@@ -47,6 +84,16 @@ def get_channel_by_title(channel_title: str, db: Session = Depends(get_db)):
 
 @app.get("/channels_by_title/{channel_title}/", response_model=list[schemas.Channel])
 def get_channels_by_title(channel_title: str, db: Session = Depends(get_db), limit: int = 3):
+    """Returns list of channels with similar to given title (using ilike query)
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_username (str): Channel username
+        limis: (int): Limits number of channels in output
+
+    Returns:
+        list(models.TelegramChannel): Returns list of channels from query result
+    """
     db_channels = crud.get_channels_by_title(db, channel_title, limit)
     if db_channels is None:
         raise HTTPException(status_code=404, detail="No channel was found")
@@ -54,6 +101,16 @@ def get_channels_by_title(channel_title: str, db: Session = Depends(get_db), lim
 
 @app.get("/channel_connections_out_after/{channel_id}/", response_model=list[schemas.Connection])
 def get_channel_connections_out_after(channel_id: int, db: Session = Depends(get_db)):
+    """Returns list of Connections going out 
+        after the split from channel provided by id
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_title (int): Channel id
+
+    Returns:
+        _type_: _description_
+    """
     db_connections = crud.get_channel_connections_out_after(db, channel_id)
     if db_connections is None:
         raise HTTPException(status_code=404, detail="No connections was found")
@@ -61,6 +118,16 @@ def get_channel_connections_out_after(channel_id: int, db: Session = Depends(get
 
 @app.get("/channel_connections_in_after/{channel_id}/", response_model=list[schemas.Connection])
 def get_channel_connections_in_after(channel_id: int, db: Session = Depends(get_db)):
+    """Returns list of Connections going in 
+        after the split from channel provided by id
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_title (int): Channel id
+
+    Returns:
+        _type_: _description_
+    """
     db_connections = crud.get_channel_connections_in_after(db, channel_id)
     if db_connections is None:
         raise HTTPException(status_code=404, detail="No connections was found")
@@ -68,6 +135,16 @@ def get_channel_connections_in_after(channel_id: int, db: Session = Depends(get_
 
 @app.get("/channel_connections_out_before/{channel_id}/", response_model=list[schemas.Connection])
 def get_channel_connections_out_before(channel_id: int, db: Session = Depends(get_db)):
+    """Returns list of Connections going out 
+        before the split from channel provided by id
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_title (int): Channel id
+
+    Returns:
+        _type_: _description_
+    """
     db_connections = crud.get_channel_connections_out_before(db, channel_id)
     if db_connections is None:
         raise HTTPException(status_code=404, detail="No connections was found")
@@ -75,6 +152,16 @@ def get_channel_connections_out_before(channel_id: int, db: Session = Depends(ge
 
 @app.get("/channel_connections_in_before/{channel_id}/", response_model=list[schemas.Connection])
 def get_channel_connections_in_before(channel_id: int, db: Session = Depends(get_db)):
+    """Returns list of Connections going in 
+        before the split from channel provided by id
+
+    Args:
+        db (Session): SQLAlchemy Session
+        channel_title (int): Channel id
+
+    Returns:
+        _type_: _description_
+    """
     db_connections = crud.get_channel_connections_in_before(db, channel_id)
     if db_connections is None:
         raise HTTPException(status_code=404, detail="No connections was found")
